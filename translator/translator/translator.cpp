d2f8b3133bc7eb0337a9aca858419c74ca46a3c7
Dmitry <puchkovdmitrybor@gmail.com>
2023-05-11 12:53:25 +0700

    lab3 sintax analizer

--- translator/translator/translator.cpp
+++ translator/translator/translator.cpp
@@ -3,6 +3,9 @@
 #include <vector>
 #include <iostream>
 #include <stdio.h>
+#include <map>
+#include <stack>
+#include <sstream>
 using namespace std;
 
 
@@ -40,13 +43,21 @@ struct buffer
    // 1=костанта
    bool type;
 };
-
+struct tableParseElem
+{
+   vector<string> terminal_; // Терминалы
+   int jump_;                // Переход
+   int accept_;              // Принимать или нет
+   int stack_;               // Cтек
+   int return_;              // Возвращать или нет
+   int error_;               // Oшибка
+};
 
 class ValueTable
 {
 public:
    const int TABLE_SIZE = 19;
-   
+
    ValueTable(int table_id)
    {
       this->table_id = table_id;
@@ -122,7 +133,7 @@ public:
    }
 
    // get elem by token
-   string get(Token token) {
+   string getElem(Token token) {
       return table[token.index];
    }
 private:
@@ -143,6 +154,10 @@ private:
 const string KeyWordsFile = "words.txt";
 const string OperationsFile = "operations.txt";
 const string DelimitersFile = "delimeters.txt";
+const string parser_output = "token_file.txt";
+const string postfix_output = "postfix.txt";
+const string errors = "errors.txt";
+const string parse_table = "parse_table.txt";
 
 
 
@@ -250,11 +265,21 @@ public:
    VariableTableElem var;
    void getElemByToken(Token token) {
       switch (token.table) {
-      case KEY_WORDS_TABLE_ID: str = keyWords.get(token); break;
-      case OPERATIONS_TABLE_ID: str = operations.get(token); break;
-      case DELIMETERS_TABLE_ID: str = delimiters.get(token); break;
+      case KEY_WORDS_TABLE_ID: str = keyWords.getElem(token); break;
+      case OPERATIONS_TABLE_ID: str = operations.getElem(token); break;
+      case DELIMETERS_TABLE_ID: str = delimiters.getElem(token); break;
       case VALUES_TABLE_ID: value = valueTable.getElem(token); break;
       case VARIABLE_TABLE_ID: var = variableTable.getElem(token); break;
+      }
+   }
+   string getElemByTokenString(Token token)
+   {
+      switch (token.table) {
+      case 0: return keyWords.getElem(token); break;
+      case 1: return operations.getElem(token); break;
+      case 2: return delimiters.getElem(token); break;
+      case 3: return to_string(valueTable.getElem(token)); break;
+      case 4: return variableTable.getElem(token).name; break;
       }
    }
 
@@ -265,7 +290,7 @@ public:
       bool is_long_comment = 0;
       buffer lexeme;
       lexeme.str = "";
-      
+
       Token delim_token;
       string operation_buffer = "";
       bool is_operation = 0;
@@ -329,14 +354,14 @@ public:
          Token token;
          if (is_operation)
          {
-            if ((token=operations.search(operation_buffer + tmp)).index != -1)
+            if ((token = operations.search(operation_buffer + tmp)).index != -1)
             {
                PrintToken(fout, token);
                operation_buffer = "";
                is_operation = 0;
                continue;
             }
-            else if ((token=operations.search(operation_buffer)).index != -1)
+            else if ((token = operations.search(operation_buffer)).index != -1)
             {
                PrintToken(fout, token);
                operation_buffer = "";
@@ -345,7 +370,7 @@ public:
          }
          //main part
 
-         if ((token=delimiters.search(string(1, tmp))).index != -1)
+         if ((token = delimiters.search(string(1, tmp))).index != -1)
          {
             PrintToken(fout, token);
             continue;
@@ -354,7 +379,7 @@ public:
 
          do
          {
-            if ((delim_token=delimiters.search(string(1, tmp))).index != -1)
+            if ((delim_token = delimiters.search(string(1, tmp))).index != -1)
             {
                is_delim = 1;
                break;
@@ -390,7 +415,7 @@ public:
             continue;
          }
 
-         if ((token=keyWords.search(lexeme.str)).index != -1)
+         if ((token = keyWords.search(lexeme.str)).index != -1)
          {
             PrintToken(fout, token);
             if (is_delim) PrintToken(fout, delim_token);
@@ -410,16 +435,455 @@ public:
       if (is_operation)
       {
          Token token;
-         if ((token=operations.search(operation_buffer)).index != -1)
+         if ((token = operations.search(operation_buffer)).index != -1)
             PrintToken(fout, token);
       }
-   } 
+   }
+
+   void automatParse(ifstream &fin, ofstream &fout) {
+      //init
+      makeP_matrix();
+      Token token;
+      char tmp;
+      int i = 0, j = 0, prev_automat = 0;
+      tmp = read_char(fin, &j);
+
+      while (j != 5)
+      {
+         string buff = "";
+         while (true)
+         {
+            if (i != 0) tmp = read_char(fin, &j);
+            prev_automat = i;
+            i = P_matrix[i][j];
+            //cout << "automat" << i << endl;
+            if (i == 9)
+            {
+               switch (prev_automat)
+               {
+               case 1:
+                  token = valueTable.findOrCreate(buff);
+                  PrintToken(fout, token);
+                  break;
+               case 2:
+                  if ((token = keyWords.search(buff)).index == -1)
+                     token = variableTable.findOrCreate(buff);
+                  PrintToken(fout, token);
+                  break;
+               case 3:
+               case 4:
+                  token = operations.search(buff);
+                  PrintToken(fout, token);
+                  break;
+               case 7:
+                  tmp = read_char(fin, &j); // чтобы слеш после коммента не учитывался
+                  break;
+               case 8:
+                  token = delimiters.search(buff);
+                  PrintToken(fout, token);
+
+               default:
+                  break;
+               }
+               break;
+            }
+            if (i == 10) {
+               switch (j)
+               {
+               case 1:
+                  fout << "unrecognized const error";
+                  break;
+               case 2:
+               case 7:
+               case 8:
+                  fout << "oprator error";
+                  break;
+               case 5:
+                  fout << "eof error";
+                  break;
+               case 6:
+                  fout << "unrecognized symbol errror";
+                  break;
+               default:
+                  fout << "unexpected error";
+                  break;
+               }
+               exit(1);
+            }
+            buff += tmp;
+         }
+         i = 0;
+      }
+
+   }
+   void parse_tokens(vector<tableParseElem> &table_)
+   {
+      ofstream postfix_stream(postfix_output);
+      ofstream error_stream(errors);
+      map<string, int> priority;
+      priority["="] = 1;
+      priority["<"] = 6;
+      priority[">"] = 6;
+      priority["!="] = 6;
+      priority["=="] = 6;
+      priority["+"] = 7;
+      priority["-"] = 7;
+      priority["*"] = 8;
+      vector<string> initialized_vars;
+      stack<int> stack_;
+      ifstream in(parser_output);
+      bool is_open;
+      int index = 1;
+      int table;
+      int hash;
+      int idx;
+      in >> table;
+      in >> hash;
+      in >> idx;
+      int depth = -1;
+      string temp_var;
+      vector<string> poland;
+      stack<string> operands;
+      Token token(table, hash, idx);
+      bool is_switch = 0;
+      int switch_depth = -1;
+      int current_L = 0;
+      int current_switch_end = 0;
+      stack<int> switch_end;
+      stack<int> l_index_stack;
+      stack<bool> was_default;
+      vector<vector<string>> poland_switch;
+      //bool was_initialized = 0;
+      string string_token;
+      string_token = getElemByTokenString(token);
+      while (index > 0)
+      {
+         cout << index << " " << string_token << " " << stack_.size() << endl;
+         if (compare_terminals(table_[index - 1].terminal_, string_token, token))
+         {
+            //switch condition
+            if (index == 23)
+            {
+               if (!is_switch)
+               {
+                  if (switch_depth == -1)
+                  {
+                     switch_depth = stack_.size();
+                  }
+                  is_switch = 1;
+               }
+               was_default.push(0);
+               switch_end.push(current_switch_end);
+               current_switch_end++;
+            }
+            //break
+            if (index == 44 || index == 39)
+            {
+               postfix_stream << 'B' << switch_end.top() << ' ' << "UJ" << ' ' << endl;
+            }
+            //next case\default
+            if ((index == 40 || index == 45) && (string_token != "}"))
+            {
+               if (string_token == "default")
+               {
+                  was_default.pop();
+                  was_default.push(1);
+               }
+               postfix_stream << 'L' << l_index_stack.top() << ':' << ' ';
+               l_index_stack.pop();
+            }
+
+            //case const
+            if (index == 28)
+            {
+               for (int switch_poland_index = 0; switch_poland_index < poland_switch[stack_.size() - switch_depth].size(); switch_poland_index++)
+               {
+                  postfix_stream << poland_switch[stack_.size() - switch_depth][switch_poland_index] << ' ';
+               }
+               postfix_stream << string_token << ' ';
+               postfix_stream << "==";
+               postfix_stream << endl;
+               postfix_stream << 'L' << current_L << ' ' << "CJ" << ' ' << endl;
+               l_index_stack.push(current_L);
+               current_L++;
+            }
+            // switch }
+            if ((index == 33) || (index == 27))
+            {
+               if (switch_depth == stack_.size())
+               {
+                  poland_switch.resize(0);
+               }
+
+               postfix_stream << 'B' << switch_end.top() << ':' << ' ';
+               switch_end.pop();
+
+               if (!was_default.top())
+               {
+                  postfix_stream << 'L' << l_index_stack.top() << ':' << ' ';
+                  l_index_stack.pop();
+               }
+
+            }
+
+            //слагаемое выражения
+            if (index == 10 || index == 14 || index == 8)
+            {
+               if (!is_initialized(initialized_vars, string_token) && token.table == VARIABLE_TABLE_ID)
+               {
+                  error_stream << "uninitialized variable";
+                  exit(-1);
+               }
+               if (depth == -1 && index != 8)
+               {
+                  depth = stack_.size();
+               }
+               if (index != 8)
+               {
+                  poland.push_back(string_token);
+               }
+            }
+            //скобки
+            if (index == 11 || index == 15)
+            {
+               if (depth == -1)
+               {
+                  depth = stack_.size();
+               }
+               is_open = 1;
+               stack_push(operands, string_token, poland, priority);
+            }
+            //операнд
+            if (index == 12 || index == 16)
+            {
+               stack_push(operands, string_token, poland, priority);
+            }
+            //закрывающая скобка но не конечная
+            if (index == 13)
+            {
+               if (depth != stack_.size())
+               {
+                  stack_push(operands, string_token, poland, priority);
+               }
+               depth = stack_.size();
+            }
+            //окончание выр-я
+            if (index == 13 || index == 17)
+            {
+               if (depth == stack_.size())
+               {
+                  //was_initialized = 0;
+                  while (!operands.empty())
+                  {
+                     poland.push_back(operands.top());
+                     operands.pop();
+                  }
+
+                  if (is_switch)
+                  {
+                     poland_switch.push_back(poland);
+                     is_switch = 0;
+                  }
+                  else
+                  {
+                     for (int poland_index = 0; poland_index < poland.size(); poland_index++)
+                     {
+                        postfix_stream << poland[poland_index] << ' ';
+                     }
+                     postfix_stream << endl;
+                  }
+                  poland.resize(0);
+                  depth = -1;
+
+               }
+            }
+
+            if (table_[index - 1].accept_ == 1)
+            {
+               in >> token.table;
+               in >> token.hash;
+               in >> token.index;
+               string_token = getElemByTokenString(token);
+               //изменение переменной
+               if (index == 18 && token.table == VARIABLE_TABLE_ID)
+               {
+                  depth = stack_.size();
+                  //poland.push_back(string_token);
+                  if (!is_initialized(initialized_vars, string_token))
+                  {
+                     //	was_initialized = 1;
+                     initialized_vars.push_back(string_token);
+                  }
+                  //operands.push("=");
+                  temp_var = string_token;
+               }
+               if (index == 19)
+               {
+                  if (string_token == "=")
+                  {
+                     poland.push_back(temp_var);
+                     stack_push(operands, string_token, poland, priority);
+                  }
+               }
+               //если она не менялась на самом деле
+               //	if (index == 22 && depth != -1)
+               //	{
+               //		was_initialized = 0;
+               //		initialized_vars.pop_back();
+               //		depth = -1;
+               //		poland.resize(0);
+               //		operands.empty();
+               //	}
+            }
+            if (table_[index - 1].stack_ == 1)
+            {
+               stack_.push(index + 1);
+            }
+            if (table_[index - 1].return_ == 1)
+            {
+               index = stack_.top();
+               stack_.pop();
+            }
+            else
+            {
+               index = table_[index - 1].jump_;
+            }
+
+         }
+         else
+         {
+            // вставить ошибку
+            if (table_[index - 1].error_ == 1)
+            {
+               error_stream << "Parsing error";
+               exit(-2);
+            }
+            else
+            {
+               index++;
+            }
+         }
+      }
+   }
 
 private:
    void PrintToken(ofstream &fout, Token token)
    {
       fout << token.table << " " << token.hash << " " << token.index << endl;
    }
+   char read_char(ifstream &fin, int *automat_idx)
+   {
+      char tmp;
+      fin.get(tmp);
+      if (!fin.good())
+      {
+         *automat_idx = 5; // EOF
+         return NULL;
+      }
+      if (tmp >= '0' && tmp <= '9') *automat_idx = 0; // цифра
+      else if (tmp >= 'A' && tmp <= 'Z' || tmp >= 'a' && tmp <= 'z') *automat_idx = 1; // буква
+      else if (tmp == '+' || tmp == '-' || tmp == '!' || tmp == '>' || tmp == '<') *automat_idx = 2; // оператор (исключая = и *)
+      else if ((delimiters.search(string(1, tmp))).index != -1) *automat_idx = 3; // разделитель
+      else if (tmp == '/') *automat_idx = 4; // слеш
+      else if (tmp == '=') *automat_idx = 7;
+      else if (tmp == '*') *automat_idx = 8;
+      else if (tmp == '\n') *automat_idx = 9;
+      else if (tmp == ' ' or tmp == '\t') *automat_idx = 10;
+      else *automat_idx = 6;
+      return tmp;
+   }
+   vector<vector<int>> P_matrix;
+   void makeP_matrix()
+   {
+      P_matrix = {
+         { 1,  2,  3,  8,  4,  9, 10,  3,  3, 11, 11},
+         { 1, 10,  9,  9,  9,  9, 10,  9,  9,  9,  9},
+         { 2,  2,  9,  9,  9,  9, 10,  9,  9,  9,  9},
+         { 9,  9,  9,  9,  9,  9, 10,  3, 10,  9,  9},
+         { 9,  9, 10,  9,  6, 10, 10,  3,  5,  9,  9},
+         { 5,  5,  5,  5,  5, 10,  5,  5,  7,  5,  5},
+         { 6,  6,  6,  6,  6,  9,  6,  6,  6,  9,  6},
+         { 5,  5,  5,  5,  9, 10,  5,  5,  7,  5,  5},
+         { 9,  9,  9,  9,  9,  9, 10,  9,  9,  9,  9},
+         {-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1},
+         {-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1},
+         { 9,  9,  9,  9,  9,  9, 10,  9,  9,  9,  9},
+      };
+
+   }
+   bool compare_terminals(vector<string> terminal_string, string string, Token token)
+   {
+      int size = terminal_string.size();
+      if (token.table == VARIABLE_TABLE_ID)
+         string = "var";
+      if (token.table == VALUES_TABLE_ID)
+         string = "const";
+      for (int i = 0; i < size; i++)
+      {
+         if (terminal_string[i] == string)
+         {
+            return 1;
+         }
+      }
+      return 0;
+   }
+
+   void stack_push(stack<string> &stack, string operand, vector<string> &poland, map<string, int> &priority)
+   {
+      if (stack.empty())
+      {
+         stack.push(operand);
+         return;
+      }
+
+      if (operand == ")")
+      {
+         while (stack.top() != "(")
+         {
+            poland.push_back(stack.top());
+            stack.pop();
+            if (stack.empty())
+               break;
+         }
+         if (!stack.empty())
+            stack.pop();
+         return;
+      }
+      if (operand == "(")
+      {
+         stack.push(operand);
+         return;
+      }
+      if (!(priority[operand] < priority[stack.top()]))
+      {
+         stack.push(operand);
+         return;
+      }
+      while (!stack.empty())
+      {
+         if (priority[operand] < priority[stack.top()])
+         {
+            poland.push_back(stack.top());
+            stack.pop();
+         }
+         else
+         {
+            break;
+         }
+      }
+      poland.push_back(operand);
+      return;
+   }
+
+   bool is_initialized(vector<string> &init_vec, string var)
+   {
+      for (int i = 0; i < init_vec.size(); i++)
+         if (init_vec[i] == var)
+            return 1;
+      return 0;
+   }
+
+
 };
 
 
@@ -428,7 +892,49 @@ int main()
    auto lexeme = Lexeme();
    ifstream fin("code.txt");
    ofstream fout("token_file.txt");
-   lexeme.Parse(fin, fout);
+   //lexeme.Parse(fin, fout);
+   lexeme.automatParse(fin, fout);
+
+
+   // чтение таблицы
+   vector<tableParseElem> table;
+   ifstream tabl(parse_table);
+   while (!tabl.eof())
+   {
+      tableParseElem tmp;
+      string string_tmp;
+      getline(tabl, string_tmp);
+      istringstream ss(string_tmp);
+      vector<string> v;
+      string t;
+      while (ss >> t)
+      {
+         v.push_back(t);
+      }
+      int string_size = v.size();
+      int count = 0;
+      while (string_size - 5 > 0)
+      {
+         tmp.terminal_.push_back(v[count]);
+         count++;
+         string_size--;
+      }
+      tmp.jump_ = stoi(v[count]);
+      count++;
+      tmp.accept_ = stoi(v[count]);
+      count++;
+      tmp.stack_ = stoi(v[count]);
+      count++;
+      tmp.return_ = stoi(v[count]);
+      count++;
+      tmp.error_ = stoi(v[count]);
+      table.push_back(tmp);
+      //std::cout << tmp.terminal_[0] << endl;
+   }
+   lexeme.parse_tokens(table);
+
+
+
    return 0;
 }
 
